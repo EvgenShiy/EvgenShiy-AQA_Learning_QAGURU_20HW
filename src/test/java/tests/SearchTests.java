3e3ae62eb481ac254502d88e1d5506e74cbd9cee
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-12-25 02:09:24 +0500

    changed

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.AppiumBy.*;
 import static io.qameta.allure.Allure.step;
@@ -21,4 +22,24 @@ public class SearchTests extends TestBase {
             $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                     .shouldHave(sizeGreaterThan(0)));
     }
+
+    @Test
+    @Tag("mobile_test")
+    void checkErrorMessageTextTest() {
+        step("Напечатать в поиске слово 'Appium'", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Открыть первую статью", () -> {
+            $$(id("org.wikipedia.alpha:id/page_list_item_title")).first().click();
+        });
+
+        step("Проверить текст сообщения об ошибке", () -> {
+            $(id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("An error occurred"));
+        });
+    }
+
+
+
+
 }
