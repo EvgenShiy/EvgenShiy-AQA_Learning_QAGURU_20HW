549cc80ec1b2805baf94f431f9cba61b0d4662bb
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-12-25 00:50:44 +0500

    changed

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -10,6 +11,7 @@ import static io.qameta.allure.Allure.step;
 public class SearchTests extends TestBase {
 
     @Test
+    @Tag("mobile_test")
     void successfulSearchTest() {
         step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
