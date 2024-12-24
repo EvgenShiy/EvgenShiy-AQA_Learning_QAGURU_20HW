6f3a15337972cb784268093eda9cc941060cb8a4
Evgeniya Shiyanova <evgenshiy@gmail.com>
2024-12-25 00:36:35 +0500

    changed

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -4,8 +4,7 @@ import org.aeonbits.owner.Config;
 
 
 @Config.Sources({
-        "classpath:properties/${env}.properties",
-//        "classpath:properties/android.properties"
+        "classpath:properties/${env}.properties"
 })
 public interface WebDriverConfig extends Config {
 
